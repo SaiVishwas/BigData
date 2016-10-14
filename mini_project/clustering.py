e3ff0539c6c52d4a32f511e6f02e471cb9928749
SaiVishwas <saivishwasp@gmail.com>
2016-10-14 13:44:31 +0530

    batsmen clustering cleaned

--- mini_project/clustering.py
+++ mini_project/clustering.py
@@ -2,93 +2,78 @@ import sys
 
 from pyspark import SparkContext, SparkConf
 
+
+def get_split_points(input_file , indices):
+  
+  tokenized = sc.textFile(input_file).flatMap(lambda line: line.split("\n"))
+  stat = tokenized.map(lambda entry: (entry, [float(entry.split(",")[indices[0]]),float(entry.split(",")[indices[1]]),int(entry.split(",")[indices[2]]),0]))
+
+  split_info = []
+  
+  for i in stat.collect() :
+    #i = [map(int, ele) for ele in i[1] ]
+    #i[1][0] = 0
+    tmp = [] 
+    tmp.append(i[1][0])
+    tmp.append(i[1][1])
+    tmp.append(i[1][2])        
+    
+    split_info.append(tmp)
+    
+  #output.write(repr(split_info))
+  
+  first_splitpoints = []
+  second_splitpoints = []
+  third_splitpoints = []
+  
+
+  split_info = sorted(split_info , key = lambda s : s[0] )
+
+  #output.write(repr(split_info))  
+
+  first_splitpoints.append(split_info[len(split_info)/2][0])
+  
+  split_info_1  = split_info[:len(split_info)/2]
+  split_info_2 = split_info[len(split_info)/2 : ]
+
+  split_info_1 = sorted(split_info_1 , key = lambda s : s[1])
+  split_info_2 = sorted(split_info_2 , key = lambda s : s[1])
+
+  second_splitpoints.append(split_info_1[len(split_info_1)/2][1])
+  second_splitpoints.append(split_info_2[len(split_info_2)/2][1])
+
+  split_info_a = split_info_1[:len(split_info_1)/2]
+  split_info_b = split_info_1[len(split_info_1)/2:]
+  
+  split_info_c = split_info_2[:len(split_info_2)/2]
+  split_info_d = split_info_2[len(split_info_2)/2:]
+
+  split_info_a = sorted(split_info_a, key = lambda s : s[2])
+  split_info_b = sorted(split_info_b, key = lambda s : s[2])
+  split_info_c = sorted(split_info_c, key = lambda s : s[2])
+  split_info_d = sorted(split_info_d, key = lambda s : s[2])
+  
+  third_splitpoints.append(split_info_a[len(split_info_a)/2][2])
+  third_splitpoints.append(split_info_b[len(split_info_b)/2][2])
+  third_splitpoints.append(split_info_c[len(split_info_c)/2][2])
+  third_splitpoints.append(split_info_d[len(split_info_d)/2][2])
+
+  return first_splitpoints , second_splitpoints , third_splitpoints
+
+
+
+
 if __name__ == "__main__":
 
   # create Spark context with Spark configuration
   conf = SparkConf().setAppName("Spark Count")
   sc = SparkContext(conf=conf)
+  output = open("output_1.txt", 'w')
 
-  tokenized = sc.textFile("batsman_input/batsmen.csv").flatMap(lambda line: line.split("\n"))
-  output = open("output.txt", 'w')
-  
-  stat = tokenized.map(lambda entry: (entry, [float(entry.split(",")[7]),float(entry.split(",")[9]),int(entry.split(",")[8]),0]))
-  
-  avg = []
-  sr = []
-  bf = []
-  
-  for i in stat.collect() :
-    #i = [map(int, ele) for ele in i[1] ]
-    #i[1][0] = 0
-    avg.append(i[1][0])
-    sr.append(i[1][1])
-    bf.append(i[1][2])        
-    
-  #output.write(repr(avg) + '\n' + repr(sr) + '\n' + repr(bf) )
-  
-  sorted_avg = sorted(avg)
-  sorted_sr = sorted(sr)
-  sorted_bf = sorted(bf)
-  
-  #output.write(repr(sorted_avg) + '\n' + repr(sorted_sr) + '\n' + repr(sorted_bf) + '\n') 
-  
-  avg_splitpoints = []
-  sr_splitpoints = []
-  bf_splitpoints = []
+  avg_splitpoints , sr_splitpoints , bf_splitpoints = get_split_points("batsman_input/batsmen.csv" , [7,9,8])
+  output.write('\n' + repr(avg_splitpoints) + '\n' + repr(sr_splitpoints) + '\n' + repr(bf_splitpoints) + '\n\n')
 
-  avg_splitpoints.append(sorted_avg[len(sorted_avg)/2])
-  
-  sr_splitpoints.append(sorted_sr[len(sorted_sr)/4])
-  sr_splitpoints.append(sorted_sr[len(sorted_sr)*3/4])   
-  
-  bf_splitpoints.append(sorted_bf[len(sorted_bf)/8])
-  bf_splitpoints.append(sorted_bf[len(sorted_bf)*3/8])
-  bf_splitpoints.append(sorted_bf[len(sorted_bf)*5/8])
-  bf_splitpoints.append(sorted_bf[len(sorted_bf)*7/8])
- 
-  output.write(repr(avg_splitpoints) + '\n' + repr(sr_splitpoints) + '\n' + repr(bf_splitpoints) ) 
-  
-  
-  
-  for i in stat.collect() :
-    #output.write('0')
-    if i[1][0] < avg_splitpoints[0] :
-      if i[1][1] < sr_splitpoints[0] :
-        if i[1][2] < bf_splitpoints[0] :
-          i[1][3] = 1
-          output.write('1')
-        else :
-          i[1][3] = 2
-          output.write('2')
-      else : 
-        if i[1][2] < bf_splitpoints[1] :
-          i[1][3] = 3
-          output.write('3')
-        else :
-          i[1][3] = 4
-          output.write('4')
-    else :
-      if i[1][1] < sr_splitpoints[1] :
-        if i[1][2] < bf_splitpoints[2] :
-          i[1][3] = 5
-          output.write('5')
-        else :
-          i[1][3] = 6
-          output.write('6')
-      else : 
-        if i[1][2] < bf_splitpoints[3] :
-          i[1][3] = 7
-          output.write('7')
-        else :
-          i[1][3] = 8      
-          output.write('8')
 
   
 
-    
-  
-  #output.write(repr(stat.collect())[:])
-  #output.write(repr(stat.collect()))
 
-  
-  
