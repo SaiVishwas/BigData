d7517269ecccc8677bf6b02ad4b719648a0a868f
SaiVishwas <saivishwasp@gmail.com>
2016-10-19 23:11:12 +0530

    display clusters

---
+++ project/phase2/print_clusters.py
@@ -0,0 +1,45 @@
+from pyspark import SparkContext, SparkConf
+from pyspark.mllib.clustering import KMeans
+from collections import defaultdict
+
+def format_input(input_file , indices) :
+  tokenized = sc.textFile(input_file).flatMap(lambda line: line.split("\n"))
+  stat = tokenized.map(lambda entry: (entry.split(",")[0] ,[float(entry.split(",")[indices[0]]),float(entry.split(",")[indices[1]]),float(entry.split(",")[indices[2]])]))
+  data = tokenized.map(lambda entry: ([float(entry.split(",")[indices[0]]),float(entry.split(",")[indices[1]]),float(entry.split(",")[indices[2]])]))
+  return [stat, data]
+
+def cluster(filename , k, indices):
+  stat, data = format_input("input/"+filename, indices)
+  model = KMeans.train(data, k)
+  cluster_centers = model.clusterCenters
+  clusters = defaultdict(list)
+
+  for x in stat.collect():
+    name = str(x[0])
+    num = int(model.predict(x[1]))
+    centers = [i for i in cluster_centers[model.predict(x[1])]]
+    clusters[num].append(name)
+    
+  return clusters,cluster_centers    
+
+def print_results(filename , clusters , cluster_centers):
+	with file("output_clusters/"+filename, "w") as f:
+		for i in clusters :
+			f.write("Cluster number : " + str(i) + "\n")
+
+			for j in clusters[i]:
+				f.write(j + "\n")
+
+			f.write("Cluster centers : " + str(cluster_centers[i]) + "\n\n")	
+
+
+if __name__ == "__main__":
+  conf = SparkConf().setAppName("Spark Count")
+  sc = SparkContext(conf=conf)
+  batsman_cluster , batsman_cluster_centers = cluster('batsmen', 8, [7, 9, 8])
+  bowler_cluster , bowler_cluster_centers = cluster('bowler', 8, [11,10,4])
+
+  print_results('batsmen' , batsman_cluster , batsman_cluster_centers)
+  print_results('bowler' , bowler_cluster , bowler_cluster_centers)
+
+  sc.stop()
